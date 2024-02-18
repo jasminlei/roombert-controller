5ad5d4bf3a138ebae04e76634dd8ae7e83d6e57a
Jasmin Leinonen <jasminleinonen@hotmail.com>
2024-02-18 08:03:14 +0000

    initial vaccuum integration

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-def button_callback(channel):
-    print("Button was pushed!")
-def button_callback2(channel):
-    print("iik!")
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(36, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 36 to be an input pin and set initial value to be pulled low (off)
-GPIO.add_event_detect(36,GPIO.RISING,callback=button_callback) # Setup event on pin 36 rising edge
-GPIO.setup(37, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 36 to be an input pin and set initial value to be pulled low (off)
-GPIO.add_event_detect(37,GPIO.RISING,callback=button_callback2) # Setup event on pin 36 rising edge
-message = input("Press enter to quit\n\n") # Run until someone presses enter
-GPIO.cleanup() # Clean up
+from miio import ViomiVacuum
+import os
+
+
+IP = os.environ.get("VACCUUM_IP_ADDRESS")
+TOKEN = os.environ.get("VACCUUM_TOKEN")
+
+vaccuum = ViomiVacuum(IP, TOKEN)
+
+
+print(vaccuum.status())
+
+
